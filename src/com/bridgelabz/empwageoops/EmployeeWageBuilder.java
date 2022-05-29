0e2b2cea2dbe2cd6c963139a4c39574cadbb8f21
a-faisalkhan <kfaisalit@gmail.com>
2022-05-29 15:57:26 +0530

    Slight use case modification

--- src/com/bridgelabz/empwageoops/EmployeeWageBuilder.java
+++ src/com/bridgelabz/empwageoops/EmployeeWageBuilder.java
@@ -41,6 +41,8 @@ public class EmployeeWageBuilder {
 	}
 
 	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the Employee wage builder program :");
 		EmployeeWageBuilder empWageBuilder = new EmployeeWageBuilder();
 		empWageBuilder.calculateEmpWage();
 	}
